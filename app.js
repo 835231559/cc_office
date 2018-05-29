9b436da6b8f4256a927c7ac40ca950ebe564b6c2
Forest_zyl <zhao835231559@126.com>
2018-05-29 22:11:03 +0800

    建立model

--- app.js
+++ app.js
@@ -8,9 +8,39 @@ const templating = require('./templating');
 
 const rest = require('./rest');
 
+const model = require('./model');
+
 const app = new Koa();
 
 const isProduction = process.env.NODE_ENV === 'production';
+
+let
+    Pet = model.Pet,
+    User = model.User;
+
+(async () => {
+    var user = await User.create({
+        name: 'John',
+        gender: false,
+        email: 'john-' + Date.now() + '@garfield.pet',
+        passwd: 'hahaha'
+    });
+    console.log('created: ' + JSON.stringify(user));
+    var cat = await Pet.create({
+        ownerId: user.id,
+        name: 'Garfield',
+        gender: false,
+        birth: '2007-07-07',
+    });
+    console.log('created: ' + JSON.stringify(cat));
+    var dog = await Pet.create({
+        ownerId: user.id,
+        name: 'Odie',
+        gender: false,
+        birth: '2008-08-08',
+    });
+    console.log('created: ' + JSON.stringify(dog));
+})();
 
 // log request URL:
 app.use(async (ctx, next) => {
